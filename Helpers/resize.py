a799caa9f0e5b617a6525c1f550e7e43b67a3c89
caroline <caroline.violot@epfl.ch>
2020-10-14 09:38:03 +0200

    add reset function

---
+++ Helpers/resize.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+from PIL import Image
+import os, sys
+
+path = "Datasets/stone_256/"
+dirs = os.listdir( path )
+
+def resize():
+    for item in dirs:
+        if os.path.isfile(path+item):
+            im = Image.open(path+item)
+            f, e = os.path.splitext(path+item)
+            imResize = im.resize((64,64), Image.ANTIALIAS)
+            imResize.save(f + '.png', 'PNG', quality=90)
+
+resize()
