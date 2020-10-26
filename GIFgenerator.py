e58d5ed46d8b80d790da24633e673868c57b60dd
caroline <caroline.violot@epfl.ch>
2020-10-26 14:45:34 +0100

    facilitates GIFgenerator use

--- GIFgenerator.py
+++ GIFgenerator.py
@@ -9,12 +9,12 @@ Created on Sat Oct 17 17:43:47 2020
 import imageio
 import os
 
-with imageio.get_writer('GeneratedImages/GANGIF2.gif', mode='I', fps = 10) as writer:
-    files = sorted(os.listdir("GeneratedImages/GAN/"))
+with imageio.get_writer('../GeneratedImages/GANGIFStone.gif', mode='I', fps = 10) as writer:
+    files = sorted(os.listdir("../GeneratedImages/GAN/Stone/"))
     
-    
-    for i in range (0, 8000, 20):#0:20: filename in files:
-        
-        image = imageio.imread("GeneratedImages/GAN/"+str(i)+".png")
-        writer.append_data(image)
+    for e in range (0, 4):
+        for i in range (0, 800, 20):#0:20: filename in files:
+            
+            image = imageio.imread("../GeneratedImages/GAN/Stone/fake_e_"+str(e)+"_"+str(i)+".png")
+            writer.append_data(image)
 
